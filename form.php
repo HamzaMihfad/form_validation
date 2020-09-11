554060e64f8d34ca2741408132c2ac946a206d87
HamzaMihfad <mihfad1hamza@gmail.com>
2020-09-11 09:24:46 +0100

    fetch results

--- form.php
+++ form.php
@@ -14,4 +14,16 @@
         $query=mysqli_query($connection,"INSERT INTO formValidation (nom, prenom, civilite) VALUES ('$nom','$prenom', '$civilite')");
     }
 
+
+
+
+
+
+
+
+    /************************/
+    // $conn = mysqli_connect('localhost','typiortx_test4job', 'test4job', 'typiortx_Test4JOB');
+    // $reslut = mysqli_query($con,"SELECT user_id, login.firstName, login.lastName, MAX(wpmSpeed) AS wpmSpeed, accValue, time, date_time FROM results, login WHERE login.id = results.user_id GROUP BY user_id ORDER BY wpmSpeed DESC");
+    // $row = mysqli_fetch_array($reslut);
+    // echo $row['firstName'];
 ?>
